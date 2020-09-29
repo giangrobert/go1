0f0889c66e955f3b28487fe3cc111938d1d75774
a sullon m <asullom@gmail.com>
2020-09-29 08:09:47 -0500

    gin

--- Go/maingin.go
+++ Go/maingin.go
@@ -6,8 +6,18 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "ponok dp o",
+			"message": "HOLA GIN",
 		})
 	})
+
+	// For each matched request Context will hold the route definition
+	r.POST("/users/", func(c *gin.Context) {
+		id := c.Query("id")
+		name := c.PostForm("name")
+		c.JSON(200, gin.H{
+			"message": name + " id=" + id,
+		})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
