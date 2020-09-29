d9c347f0e6cb3ce2186ee1e17acc3dbc02844bec
a sullon m <asullom@gmail.com>
2020-09-29 09:33:29 -0500

    Person table

--- Go/maingin.go
+++ Go/maingin.go
@@ -1,8 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	//"model"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Person struct {
+	gorm.Model
+	Name string
+	Age  int
+}
 
 func main() {
+	dsn := "docker:docker@tcp(db:3306)/test_db?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Person{})
+
+	//s := Person{Name: "Sean", Age: 50}
+	//s.Name = "Sean"
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
