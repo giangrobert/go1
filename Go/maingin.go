6d414ab9d82bf7193be368ce01cc148bf229af55
giangrobert <gianmamani93@gmail.com>
2020-10-03 22:05:36 -0500

    Crud GIN GORM

--- Go/maingin.go
+++ Go/maingin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	//"model"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
@@ -35,14 +36,65 @@ func main() {
 		})
 	})
 
-	r.POST("/persons/", func(c *gin.Context) {
-		d := Person{Name: c.PostForm("name"), Age: c.PostForm("age")}
-		db.Create(&d)
+	r.GET("/persons/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var d Person
+		if err := db.First(&d, id).Error; err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		//db.First(&d, id)
+		//c.BindJSON(&d)
+		c.JSON(http.StatusOK, &d)
+	})
 
-		c.JSON(200, gin.H{ // serializador de gin
-			"name": d.Name,
-			"age":  d.Age,
-		})
+	r.GET("/persons", func(c *gin.Context) {
+		var lis []Person
+		db.Find(&lis)
+		c.JSON(http.StatusOK, lis)
+	})
+
+	r.POST("/persons/", func(c *gin.Context) {
+		//var d Person
+		d := Person{Name: c.PostForm("name"), Age: c.PostForm("age")}
+		/*if err := c.BindJSON(&d); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}*/
+		db.Create(&d)
+		c.JSON(http.StatusOK, &d)
+	})
+
+	r.PUT("/persons/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var d Person
+		if err := db.First(&d, id).Error; err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		d.Name = c.PostForm("name")
+		d.Age = c.PostForm("age")
+		//c.BindJSON(&d)
+		db.Save(&d)
+		c.JSON(http.StatusOK, &d)
+	})
+
+	r.DELETE("/persons/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var d Person
+		if err := db.Where("id = ?", id).First(&d).Error; err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		db.Unscoped().Delete(&d)
 	})
 
 	r.Run()
